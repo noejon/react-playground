09a008d4be59ad59217faba7d116b2ad3313252e
JonathanN <JonathanN@nextgen.nextgen.net>
2018-02-06 14:44:58 +1100

    Added the person component
    Added the person component, which so far just displays I am a person

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
+        <p>Test</p>
+        <Person/>
       </div>
     );
   }
