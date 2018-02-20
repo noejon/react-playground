43e69ab9a6751b7c2dd26062b4788db6d55a7af6
JonathanN <JonathanN@nextgen.nextgen.net>
2018-02-20 12:23:23 +1100

    Getting started on state and props
    Getting started on how props and state works.
    Saw what is the difference between class and function. Function can't have a state.
    Added notes on what is doing what.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // state is only available in classes that extends Component
+  // and they are not available to function components such as Person
+  // Fucntion components should be used as much as possible
+  state = {
+    persons: [
+      { name: 'Jon', age: 32 },
+      { name: 'Meng', age: 30 }
+    ]
+  };
+
+  /**
+   * A state change will lead to the DOM to be refreshed. Only changes in state and props trigger React to re-render your components and update the DOM
+   */
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>Test</p>
-        <Person/>
+        <button>Switch names</button>
+        {/* <Person name="Jon" age="32">My Hobbies: Le Racing</Person>
+        <Person name="Meng" age="30"/> */}
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Le Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {Component}  from 'react';
 
-const person = () => {
-  return <p> I am a person</p>
-}
+const person = (props) => {
+  // return <p> I am a person and I am {Math.floor(Math.random() * 30)}</p>
+  // return <p>I am {props.name} and I am {props.age} years old.</p>
+  return (
+    <div>
+      <p>I am {props.name} and I am {props.age} years old.</p>
+      {/* props.children is a reserverd word, and it yields whatever is in between the opening
+      and closing html tags */}
+      <p>{props.children}</p>
+    </div>
+  );
+};
 
 export default person;
