fbd261506a1f0dfa9f7a563213118e3fda2c9941
JonathanN <JonathanN@nextgen.nextgen.net>
2018-02-20 16:32:52 +1100

    Implemented a start of 2 way binding
    Two way binding with Meng's name being two way binded in the example.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Person from './Person/Person';
 class App extends Component {
   // state is only available in classes that extends Component
   // and they are not available to function components such as Person
-  // Fucntion components should be used as much as possible
+  // Function components should be used as much as possible
   state = {
     persons: [
       { name: 'Jon', age: 32 },
@@ -17,16 +17,48 @@ class App extends Component {
    * A state change will lead to the DOM to be refreshed. Only changes in state and props trigger React to re-render your components and update the DOM
    */
 
+   // The function is defined as follow to avoid having to bind it in the constructor.
+   // If it was defined as an ES6 function, it could not be called using the this of the class.
+  switchNameHandler = (newName) => {
+    // DO NOT DO THIS: this.state.persons[0].state.name = 'Jonathan';
+    // Use setState instead
+    this.setState({
+      persons: [
+        { name: newName, age: 32 },
+        { name: 'Meng', age: 19 }
+      ] 
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Jon', age: 32 },
+        { name: event.target.value, age: 19 }
+      ] 
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>Test</p>
-        <button>Switch names</button>
+        {/* Use bind to pass parameters to the function */}
+        <button onClick={ this.switchNameHandler.bind(this, 'Jono') }>Switch names</button>
         {/* <Person name="Jon" age="32">My Hobbies: Le Racing</Person>
         <Person name="Meng" age="30"/> */}
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Le Racing</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        {/* Another way to pass parameters to the function would be using an ES6 arrow function. Bind is the preferred method though (TODO: Google why) */}
+        <Person
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+          switch={() => this.switchNameHandler('Jonathan') }
+        >My Hobbies: Le Racing</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          change={this.nameChangedHandler}
+        />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,25 @@
-import React, {Component}  from 'react';
+import React  from 'react';
+
+
+/**
+ *  Application state should only be changed in containers. Use function returning JSX as often as possible
+ */
 
 const person = (props) => {
   // return <p> I am a person and I am {Math.floor(Math.random() * 30)}</p>
   // return <p>I am {props.name} and I am {props.age} years old.</p>
   return (
-    <div>
+    // Methods can be passed as props
+    <div onClick={props.switch ? props.switch : null}>
       <p>I am {props.name} and I am {props.age} years old.</p>
       {/* props.children is a reserverd word, and it yields whatever is in between the opening
       and closing html tags */}
       <p>{props.children}</p>
+      <input
+        type="text"
+        onChange={props.change}
+        value={props.name}
+      />
     </div>
   );
 };
